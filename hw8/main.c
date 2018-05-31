21bc7a5052d8e23253a7e4fd440df8df48a0adad
Alexander <ratatuy.1995@mail.ru>
2018-05-31 03:10:43 +0300

    add to hw8 dump mem

--- hw8/main.c
+++ hw8/main.c
@@ -10,8 +10,9 @@
 #define handler_error(msg) \
 		{perror(msg); exit(1);}
 
-void posix_signal(int signum, siginfo_t *info, void* uc_void) {
 
+
+void posix_signal(int signum, siginfo_t *info, void* uc_void) {
 	ucontext_t* uc = (ucontext_t *)uc_void;
 
 	void * call_addr;
@@ -24,9 +25,9 @@ void posix_signal(int signum, siginfo_t *info, void* uc_void) {
 #elif defined(__x86_64__)
 	call_addr = (void *)uc->uc_mcontext.gregs[REG_RIP];
 #else
-#error Unsupported arc
+#error Unsupported compiler
 #endif
-
+	fprintf(stderr, "_____REG___DUMP_____\n\n");
 	fprintf(stderr, "signal %d (%s), address is %p from %p\n", 
 		signum, strsignal(signum), info->si_addr, call_addr);
 
@@ -40,13 +41,24 @@ void posix_signal(int signum, siginfo_t *info, void* uc_void) {
 	if(mess == NULL) {
 		handler_error("backtrace_symbols");
 	}
-	
+	fprintf(stderr, "\n\n_____BACKTRACE_____\n\n");
 	for(int i = 1; i < size && mess != NULL; i++) {
 		fprintf(stderr, "[bt]: (%d) %s\n",i, mess[i]);
 
 	}
 	free(mess);
 
+
+	char* mem_addr_it = (char *)info->si_addr - 5;	
+
+	char* mem_addr_fin = mem_addr_it + 10;	
+
+	//printf("%p\n", mem_addr);
+	fprintf(stderr, "\n\n_____MEMORY___DUMP_____\n\n");
+	while(mem_addr_it != mem_addr_fin) {
+		fprintf(stderr, "address 0x%zx\n", (size_t)mem_addr_it++);
+	}
+	fprintf(stderr, "_____END______\n");
 	exit(3);
 
 }
@@ -54,7 +66,7 @@ void posix_signal(int signum, siginfo_t *info, void* uc_void) {
 void fall() {
 	int p[10];
 
-	p[1000000] = 13;
+	p[20000] = 13;
 
 }
 
@@ -69,6 +81,8 @@ void foo1() {
 
 int main() {
 	struct sigaction act;
+
+	//memset(&act, 0, sizeof(act));
 
 	act.sa_flags = SA_SIGINFO;
 	act.sa_sigaction = posix_signal;
